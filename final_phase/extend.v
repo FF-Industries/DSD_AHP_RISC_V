68c64ee320b470ef9bb470508bd2b0456a2367ec
Gautham Varma K <k.gauthamvarma@gmail.com>
2023-11-05 19:01:00 +0530

    :shoe: Little polish work

--- final_phase/extend.v
+++ final_phase/extend.v
@@ -14,7 +14,7 @@ module extend (
       2'b10:   immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
       // J-type (jal)
       2'b11:   immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-      default: immext = 32'bx;  // undefined
+      default: immext = {32{1'bx}};  // undefined
     endcase
   end
 
